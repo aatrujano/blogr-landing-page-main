4015af2b8ddebb02654e037242734aeb4277f928
aatrujano <76922673+aatrujano@users.noreply.github.com>
2021-12-27 11:26:33 -0600

    dropdown menus desktop&mobile

--- js/main.js
+++ js/main.js
@@ -1,13 +1,17 @@
 /*DROPDOWN MENUS*/
 const body = document.getElementById('body');
+const overlay = document.getElementById('overlay');
+const menu = document.getElementById('menu');
 const product = document.getElementById(1);
 const company = document.getElementById(2);
 const connect = document.getElementById(3);
 const productDropdown = document.getElementById('productDropdown');
 const companyDropdown = document.getElementById('companyDropdown');
 const connectDropdown = document.getElementById('connectDropdown');
+const hamburgerIcon = document.getElementById('hamburger');
 let dropDownArr = [productDropdown, companyDropdown, connectDropdown];
 let menuArr = [product, company, connect];
+
 const addClass = (tag, className) => {
   tag.classList.add(className);
 };
@@ -25,35 +29,24 @@ const showDropDown = ({ target }) => {
       : null
   );
 
-  target.classList.contains('selectedMenu')
-    ? removeClass(target, 'selectedMenu')
-    : addClass(target, 'selectedMenu');
-
-  const displayed = dropDownArr[id - 1];
-  const removed = dropDownArr.filter((dropdown) => dropdown !== displayed);
-  removed.forEach((div) =>
-    div.classList.contains('displayMenu')
-      ? removeClass(div, 'displayMenu')
-      : null
-  );
-  displayed.classList.contains('displayMenu')
-    ? removeClass(displayed, 'displayMenu')
-    : addClass(displayed, 'displayMenu');
-};
-
-const hideDropDown = ({ target }) => {
-  target.classList.contains('displayMenu')
-    ? removeClass(target, 'displayMenu')
-    : null;
-
-  menuArr.forEach((menu) =>
-    menu.classList.contains('selectedMenu')
-      ? removeClass(menu, 'selectedMenu')
-      : null
-  );
+  target.classList.toggle('selectedMenu');
+  overlay.classList.toggle('showOverlay');
 };
 
 menuArr.forEach((menu) => menu.addEventListener('click', showDropDown));
-dropDownArr.forEach((dropdown) =>
-  dropdown.addEventListener('mouseleave', hideDropDown)
-);
+hamburgerIcon.addEventListener('click', () => {
+  hamburgerIcon.classList.toggle('open');
+  menu.classList.toggle('showMobileMenu');
+  console.log(menu);
+});
+
+// OVERLAY
+
+overlay.addEventListener('click', () => {
+  menuArr.forEach(menu => {
+    if(menu.classList.contains('selectedMenu')){
+      removeClass(menu, 'selectedMenu');
+    }
+  })
+  overlay.classList.toggle('showOverlay');
+})
